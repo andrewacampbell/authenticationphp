8208fbf1f2311c8777ac66c193ad997eae584662
andrewacampbell <legero_cam@yahoo.com>
2017-02-05 09:29:40 -0500

    start working on login page

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
+<?php include_once 'resources/database.php'?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <h2>User auth system</h2>
 
-echo "hello new world";
+    <p> You are currently not signin <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </p>
 
- ?>
+    <p> You are logged in as {username here}<a href="logout.php"> Logout </a> </p>
+
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,17 @@
+<?php include_once 'resources/database.php'?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <h2>User auth system</h2>
+
+    <h3> Login form</h3>
+    
+
+    <p><a href="index.php"> Back </a> </p>
+
+  </body>
+</html>
