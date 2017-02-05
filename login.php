cdb95249e9e69f70be776c52595722e6f943b11a
andrewacampbell <legero_cam@yahoo.com>
2017-02-05 09:55:03 -0500

    added login form to login page

--- login.php
+++ login.php
@@ -9,7 +9,20 @@
     <h2>User auth system</h2>
 
     <h3> Login form</h3>
-    
+
+    <form class="form-inline">
+              <label class="sr-only" for="inlineFormInput">Name</label>
+              <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Jane Doe">
+
+              <label class="sr-only" for="inlineFormInputGroup">Username</label>
+              <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                <div class="input-group-addon"></div>
+                <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+              </div>
+
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
     <p><a href="index.php"> Back </a> </p>
 
